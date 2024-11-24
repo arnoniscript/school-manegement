0fe3e4a9de88374281bfcb9a7c95e9e3e98d666d
Luiz Arnoni <luiz.arnoni@gmail.com>
2024-11-24 17:24:19 -0300

    Feat: models

---
+++ school-management/app/Models/Course.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    public function enrollments()
+    {
+        return $this->hasMany(Enrollment::class);
+    }
+}
